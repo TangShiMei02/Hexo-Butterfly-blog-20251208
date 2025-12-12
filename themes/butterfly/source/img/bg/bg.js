8394190cd92e06a07e6a144c3dda294d10d52b31
TangShiMei02 <2244182951@qq.com>
2025-12-12 13:57:34 +0800

    update

--- themes/butterfly/source/img/bg/bg.js
+++ themes/butterfly/source/img/bg/bg.js
@@ -1,6 +1,6 @@
 /* themes/butterfly/source/img/bg/bg.js */
 (function(){
-  const TOTAL = 3;                       // 几张图就写几
+  const TOTAL = 1;                       // 几张图就写几
   const idx   = Math.floor(Math.random()*TOTAL)+1;
   const url   = '/img/bg/bg'+idx+'.png';
   document.body.style.cssText +=
